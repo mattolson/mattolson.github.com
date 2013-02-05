4c257ef9a53c900498837692c341638cfbc2751b
Matt Olson <matt@mattolson.com>
2013-02-04 19:35:05 -0800

    A bunch of style changes
    -- Add a real blog post and remove example post
    -- Fix blog feed
    -- Add background image
    -- Add tiny profile image for top nav
    -- Add jquery.backstretch for hero banner image
    -- Add Open Sans font from Google Web Fonts
    -- Update main layout and css quite a bit
    -- Add forgery gem for test data

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ gem "kramdown"
 gem "builder", "~> 3.0.0"
 gem "compass"
 gem "zurb-foundation", :github => 'zurb/foundation' # until 3.2.6 comes out
+gem "forgery"
--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -2,6 +2,7 @@
 //= require foundation
 //= require jquery.fancybox
 //= require jquery.fancybox-media
+//= require jquery.backstretch
 //= require imgsizer
 //= require respond
 
